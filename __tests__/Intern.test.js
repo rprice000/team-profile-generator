c9bca8ff3f2d603b7e72123b58216728ebbeb2ca
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-10-29 14:57:56 -0500

    success fail intern test

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,12 @@
+const Intern = require('../lib/Intern');
+
+// test to create intern object
+test('test that creates an intern object', () => {
+    // random variable intern to test
+            const intern = new Intern('Reagan', 10000, 'rtprice21@gmail.com', 'UH');
+    // Creates tests that checks if name, email, and school are strings and id is a number
+            expect(intern.name).toEqual(expect.any(String));
+            expect(intern.id).toEqual(expect.any(Number));
+            expect(intern.email).toEqual(expect.any(String));
+            expect(intern.school).toEqual(expect.any(String));
+        });
