fcba0a0392c7c86a4f182994746e06c4bba6b4dd
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-10-29 13:28:42 -0500

    successful bad test for employee object

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,13 @@
+// Dependency statements
+const Employee = require("../lib/Employee");
+
+
+// test to create employee object
+test('test that creates an employee object', () => {
+// random variable employee to test
+    const employee = new Employee('Reagan', 10000, 'rtprice21@gmail.com');
+// Creates tests that checks if name and email are strings and id is a number
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
