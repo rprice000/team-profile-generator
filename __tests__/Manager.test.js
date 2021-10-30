9061e66be47a61c8af77a5e5daef2c5718f61905
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-10-29 20:53:44 -0500

    successful failed manager object test

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,13 @@
+const Manager = require("../lib/Manager");
+
+
+// test to create manager object
+test('test that creates a manager object', () => {
+    // random variable manager to test
+            const manager = new manager('Reagan', 10000, 'rtprice21@gmail.com', 200);
+    // Creates tests that checks if name and email are strings and id and office number are numbers
+            expect(manager.name).toEqual(expect.any(String));
+            expect(manager.id).toEqual(expect.any(Number));
+            expect(manager.email).toEqual(expect.any(String));
+            expect(manager.officeNumber).toEqual(expect.any(Number));
+        });
