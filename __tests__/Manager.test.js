97897c7f54d9936773e0ca5c447a603510f6e9da
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-10-29 20:58:05 -0500

    successful passed Intern object test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -4,7 +4,7 @@ const Manager = require("../lib/Manager");
 // test to create manager object
 test('test that creates a manager object', () => {
     // random variable manager to test
-            const manager = new manager('Reagan', 10000, 'rtprice21@gmail.com', 200);
+            const manager = new Manager('Reagan', 10000, 'rtprice21@gmail.com', 200);
     // Creates tests that checks if name and email are strings and id and office number are numbers
             expect(manager.name).toEqual(expect.any(String));
             expect(manager.id).toEqual(expect.any(Number));
