3e0f120caee5329980627719e48626d718ae156a
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-10-31 16:42:41 -0500

    Verified that question from prompt work properly

--- index.js
+++ index.js
@@ -102,75 +102,6 @@ const teamMembers = () => {
     })
 }
 
-
-// function createNewEmployee() {
-//     return inquirer.prompt ([
-//         {
-//             type: 'list',
-//             name: 'role',
-//             message: "Please choose your employee's role",
-//             choices: ['Engineer', 'Intern', 'Manager']
-//         },
-//         {
-//             type: 'input',
-//             name: 'name',
-//             message: "What's the name of the employee?", 
-//         },
-//         {
-//             type: 'input',
-//             name: 'id',
-//             message: "Please enter the employee's ID.",
-//         },
-//         {
-//             type: 'input',
-//             name: 'email',
-//             message: "Please enter the employee's email.",
-//         },
-//         {
-//             type: 'input',
-//             name: 'github',
-//             message: "Please enter the employee's github username.",
-//             when: (input) => input.role === "Engineer",
-//         },
-//         {
-//             type: 'input',
-//             name: 'school',
-//             message: "Please enter the intern's school",
-//             when: (input) => input.role === "Intern",
-//         },
-//         {
-//             type: 'input',
-//             name: 'officeNumber',
-//             message: "Please enter the manager's office number",
-//             when: (input) => input.role === "Manager",
-//         },
-//         {
-//             type: 'confirm',
-//             name: 'confirmAddEmployee',
-//             message: 'Would you like to add more team members?',
-//             default: false
-//         }
-//     ])
-//     .then(userInput => {
-//         let { role, name, id, email, github, school, officeNumber, confirmAddEmployee } = userInput;
-//         let newEmployee;
-
-//         if (role === "Engineer") {
-//             newEmployee = new Engineer (name, id, email, github);
-//             console.log(newEmployee);
-//         }
-//         else if (role === "Intern" ) {
-//             newEmployee = new Intern (name, id, email, school);
-//             console.log(newEmployee);
-//         }
-//         else if (role === "Manager" ) {
-//             newEmployee = new Manager (name, id, email, officeNumber);
-//             console.log(newEmployee);
-//         }
-    
-//     })
-// };
-
 const writeFile = answers => {
     fs.writeFile('./dist/index.html', answers, err => {
         if (err) {
@@ -185,6 +116,9 @@ const writeFile = answers => {
 
 teamManager()
     .then(teamMembers)
+    .then(proiflesArray => {
+        return generateHTML(proiflesArray);
+    })
     .then(profilesHTML => {
         return writeFile(profilesHTML);
     })
