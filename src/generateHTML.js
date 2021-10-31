84a1fdc7affdfaf177ce0f2416fc202d16931d56
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-10-31 15:19:45 -0500

    added employee cards to generateHTML.js

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,5 +1,57 @@
+// creates card based on Engineer Object Data
+const createEngineer = function (newEngineer) {
+  return `
+  <div>
+    <h3>${newEngineer.name}</h3>
+    <h4>Engineer</h4>
+  </div>
+  <div>
+    <p>ID: ${newEngineer.id}</p>
+    <p>Email: ${newEngineer.email}</p>
+    <p>Github: ${newEngineer.github}</p>
+  </div>
+  `
+};
+
+// creates card based on Intern Object Data 
+const createIntern = function (newIntern) {
+  return `
+  <div>
+    <h3>${intern.name}</h3>
+    <h4>Intern</h4>
+  </div>
+  <div>
+    <p>ID: ${intern.id}</p>
+    <p>Email: ${intern.email}</p>
+    <p>School: ${intern.school}</p>
+  </div>
+  `
+};
+
+// creates card based on Manager Object Data
+const createManager = function (newManager) {
+  return `
+  <div>
+    <h3>${manager.name}</h3>
+    <h4>Manager</h4>
+  </div>
+  <div>
+    <p>ID: ${manager.id}</p>
+    <p>Email: ${manager.email}</p>
+    <p>Office Number: ${manager.officeNumber}</p>
+  </div>
+  `
+};
+
+
+
+
+
+
+
+
 const teamProfilePage = function (profiles) {
-    return `
+  return `
 <!DOCTYPE html>
   <html lang="en">
   <head>
