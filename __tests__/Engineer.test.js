bf346aa7be06042f0475d0b618a016905777458c
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-10-31 14:40:50 -0500

    added notes to test files

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,13 +1,14 @@
+// Dependency statements
 const Engineer = require('../lib/Engineer');
 
 
 // test to create engineer object
 test('test that creates an engineer object', () => {
 // random variable engineer to test
-        const engineer = new Engineer('Reagan', 10000, 'rtprice21@gmail.com', 'rprice000');
+          const engineer = new Engineer('Reagan', 10000, 'rtprice21@gmail.com', 'rprice000');
 // Creates tests that checks if name, email, and github are strings and id is a number
-        expect(engineer.name).toEqual(expect.any(String));
-        expect(engineer.id).toEqual(expect.any(Number));
-        expect(engineer.email).toEqual(expect.any(String));
-        expect(engineer.github).toEqual(expect.any(String));
-    });
+          expect(engineer.name).toEqual(expect.any(String));
+          expect(engineer.id).toEqual(expect.any(Number));
+          expect(engineer.email).toEqual(expect.any(String));
+          expect(engineer.github).toEqual(expect.any(String));
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,13 +1,14 @@
+// Dependency statements
 const Manager = require("../lib/Manager");
 
 
 // test to create manager object
 test('test that creates a manager object', () => {
-    // random variable manager to test
+// random variable manager to test
             const manager = new Manager('Reagan', 10000, 'rtprice21@gmail.com', 200);
-    // Creates tests that checks if name and email are strings and id and office number are numbers
+// Creates tests that checks if name and email are strings and id and office number are numbers
             expect(manager.name).toEqual(expect.any(String));
             expect(manager.id).toEqual(expect.any(Number));
             expect(manager.email).toEqual(expect.any(String));
             expect(manager.officeNumber).toEqual(expect.any(Number));
-        });
+});
